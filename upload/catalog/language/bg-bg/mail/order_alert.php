01adccd7e15f7bf7ca8563c75dae272eba20bd39
ergobg <69973815+ergobg@users.noreply.github.com>
2020-08-20 17:04:26 +0300

    Add files via upload

---
+++ upload/catalog/language/bg-bg/mail/order_alert.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_subject']      = '%s - Поръчка %s';
+$_['text_received']     = 'Получихте поръчка.';
+$_['text_order_id']     = 'Поръчка ID:';
+$_['text_date_added']   = 'Дата на добавяне:';
+$_['text_order_status'] = 'Статус на поръчка:';
+$_['text_product']      = 'Продукти';
+$_['text_total']        = 'Общо';
+$_['text_comment']      = 'Коментари към поръчката:';
